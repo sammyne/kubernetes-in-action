56bf359badc055849d45de789fb385db5237fb10
Sammy <lixiangmin01@baidu.com>
2019-08-22 20:26:30 +0800

    feature(chapter06): done reviewing

--- docker/fortune/fortuneloop.sh
+++ docker/fortune/fortuneloop.sh
@@ -5,7 +5,7 @@ mkdir /var/htdocs
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
-  /usr/games/fortune > /var/htdocs/index.html
+  fortune > /var/htdocs/index.html
   sleep 10
 done
 
