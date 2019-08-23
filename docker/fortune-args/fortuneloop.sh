40dc93807510dbe6278a5dc0b1bde667cd0e8fc9
Sammy <lixiangmin01@baidu.com>
2019-08-23 16:09:08 +0800

    feature(chapter07): done reviewing

--- docker/fortune-args/fortuneloop.sh
+++ docker/fortune-args/fortuneloop.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 trap "exit" SIGINT
-
 INTERVAL=$1
 echo Configured to generate new fortune every $INTERVAL seconds
-
-mkdir -p /var/htdocs
+mkdir /var/htdocs
 
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
-  /usr/games/fortune > /var/htdocs/index.html
+  fortune > /var/htdocs/index.html
   sleep $INTERVAL
 done
 
--- docker/fortune-env/fortuneloop.sh
+++ docker/fortune-env/fortuneloop.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 trap "exit" SIGINT
-
 echo Configured to generate new fortune every $INTERVAL seconds
-
-mkdir -p /var/htdocs
+mkdir /var/htdocs
 
 while :
 do
   echo $(date) Writing fortune to /var/htdocs/index.html
-  /usr/games/fortune > /var/htdocs/index.html
+  fortune > /var/htdocs/index.html
   sleep $INTERVAL
 done
 
